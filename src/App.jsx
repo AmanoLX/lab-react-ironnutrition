975db18ce44572fb4f3da88d2353c617e7c7f12c
Michel Ribbens <michelribbens@gmail.com>
2021-03-09 17:30:58 +0000

    iteration 3

--- src/App.jsx
+++ src/App.jsx
@@ -3,17 +3,41 @@ import './App.scss';
 import AddNewMeal from './components/AddNewMeal';
 import MealBox from './components/MealBox';
 import meals from './meals.json';
+import Button from 'react-bootstrap/Button';
 
 class App extends Component {
 	state = {
 		meals,
+		active: false,
+	};
+
+	toggleForm = () => {
+		this.setState({
+			active: !this.state.active,
+		});
+	};
+
+	addNewMeal = meal => {
+		this.setState({
+			meals: [meal, ...this.state.meals],
+		});
+		this.toggleForm();
 	};
 
 	render() {
 		return (
 			<main>
 				<section className='container'>
-					<AddNewMeal />
+					<Button
+						className='mb-3'
+						variant='primary'
+						type='submit'
+						block
+						onClick={this.toggleForm}>
+						Add New Meal
+					</Button>
+					{this.state.active && <AddNewMeal onAddNewMeal={this.addNewMeal} />}
+
 					{meals.map(meal => (
 						<MealBox
 							key={Math.random().toString()}
--- src/components/AddNewMeal.jsx
+++ src/components/AddNewMeal.jsx
@@ -4,67 +4,72 @@ import Button from 'react-bootstrap/Button';
 
 class AddNewMeal extends Component {
 	state = {
-		active: false,
 		name: '',
 		calories: '',
 		image: '',
 	};
 
-	toggleForm = () => {
+	handleInput = e => {
+		let { name, value } = e.target;
 		this.setState({
-			active: !this.state.active,
+			[name]: value,
+		});
+	};
+
+	handleSubmission = e => {
+		e.preventDefault();
+		const addNewMeal = this.state;
+		this.props.onAddNewMeal(addNewMeal);
+		// close and clear the form
+		this.setState({
+			name: '',
+			calories: '',
+			image: '',
 		});
 	};
 
 	render() {
-		const { image, name, calories } = this.state;
+		const { name, calories, image } = this.state;
 		return (
 			<div>
-				<Button
-					className='mb-3'
-					variant='primary'
-					type='submit'
-					block
-					onClick={this.toggleForm}>
-					Add New Meal
-				</Button>
-				{this.state.active && (
-					<Form onSubmit={this.handleSubmission} className='mb-5'>
-						{/* Name Input */}
-						<Form.Group>
-							<Form.Label>Name</Form.Label>
-							<Form.Control
-								type='text'
-								placeholder='Name'
-								onChange={this.handleNameChange}
-								value={name}
-							/>
-						</Form.Group>
-						{/* Calories Input */}
-						<Form.Group>
-							<Form.Label>Calories</Form.Label>
-							<Form.Control
-								type='text'
-								placeholder='Calories'
-								onChange={this.handleCaloriesChange}
-								value={calories}
-							/>
-						</Form.Group>
-						{/* Image Input */}
-						<Form.Group>
-							<Form.Label>Image</Form.Label>
-							<Form.Control
-								type='text'
-								placeholder='Image'
-								onChange={this.handleImageChange}
-								value={image}
-							/>
-						</Form.Group>
-						<Button variant='primary' type='submit' block>
-							Submit
-						</Button>
-					</Form>
-				)}
+				<Form onSubmit={this.handleSubmission} className='mb-5'>
+					{/* Name Input */}
+					<Form.Group>
+						<Form.Label>Name</Form.Label>
+						<Form.Control
+							type='text'
+							name='name'
+							placeholder='Name'
+							onChange={this.handleInput}
+							value={name}
+						/>
+					</Form.Group>
+					{/* Calories Input */}
+					<Form.Group>
+						<Form.Label>Calories</Form.Label>
+						<Form.Control
+							type='number'
+							name='calories'
+							placeholder='Calories'
+							onChange={this.handleInput}
+							value={calories}
+						/>
+					</Form.Group>
+					{/* Image Input */}
+					<Form.Group>
+						<Form.Label>Image</Form.Label>
+						<Form.Control
+							type='text'
+							name='image'
+							placeholder='Image'
+							onChange={this.handleInput}
+							value={image}
+						/>
+					</Form.Group>
+					<Button variant='primary' type='submit' block>
+						Submit
+					</Button>
+				</Form>
 			</div>
 		);
 	}
